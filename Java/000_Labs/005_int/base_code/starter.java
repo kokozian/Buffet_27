b643d91f73f90f535988af8fdfa64973c5412ae0
Carlo <70654159+kokozian@users.noreply.github.com>
2026-09-14 18:51:25 +0000

    a

--- Java/000_Labs/005_int/base_code/starter.java
+++ Java/000_Labs/005_int/base_code/starter.java
@@ -9,6 +9,11 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		int hi = 5*7/2%3-1
+		int eq1 = 5*7/2%3-1;
+		int eq2 = 10%3-4*7+2;
+		int eq3 = (3-7/2*5)%10;
+		System.out.println(eq1);
+		System.out.println(eq2);
+		System.out.println(eq3);
 	}
 }
--- Java/000_Labs/006_doubles/base_code/starter.java
+++ Java/000_Labs/006_doubles/base_code/starter.java
@@ -9,6 +9,9 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.print("I love to learn coding remotely.");
+		int F = 100 ;
+		double C = (F-32.0)*5/9;
+		System.out.println(C);
 	}
 }

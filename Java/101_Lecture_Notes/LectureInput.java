84d15c1f4bba2f8aaa02f0343e0c5cc8737134b6
Carlo <70654159+kokozian@users.noreply.github.com>
2026-09-15 17:52:50 +0000

    u

--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -1,9 +1,25 @@
 /*
-    Lecture note example - Input!!
+   
+   
 */
+import java.util.Scanner;
 
 class LectureInput{
     public static void main(String args[]) {
-        // Your Code Goes here!
+        double price1 = 3.00
+        double price2 = 4.50
+        double price3 =         
+        System.out.println("Welcome to Carlo's Burger Shop");
+        System.out.println("Hamburger"+ -price1 );
+        System.out.println("Cheeseburger"+ -price2 );
+        System.out.println("Fountian Drink"+ -price3 );
+        System.out.println("Whats the name for the order?");
+
+        System.out.println("How many hamburger's would you like");
+        Scanner sc = new scanner(System.in);
+
+        int price1Quant = sc.nextInt();
+        System.out.println("The total for that will be" price1*price1Quant)
+
 	}
 }

a6313eba3e60c1634b9b87b3dd195318790c8ac0
Carlo <70654159+kokozian@users.noreply.github.com>
2026-09-18 17:50:04 +0000

    u

--- Java/000_Labs/009_math_methods/base_code/starter.java
+++ Java/000_Labs/009_math_methods/base_code/starter.java
@@ -9,10 +9,17 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		Scanner sc = new Scanner(System.in);
-		double number = sc.nextDouble();
-		Math.method();
-		Math.max(13-6*11,30%7*(-2));
-		
+		double m = Math.max(13-6*11,30%7*(-2));
+		System.out.println(m);
+		double s = Math.sqrt(3*8+31%7);
+		System.out.println(s);
+		double p = Math.pow(37/3,35%21);
+		System.out.println(p);
+		double all = Math.max(Math.pow(2,14%3),Math.sqrt(2*6));
+		System.out.println(all);
+
+		//Extra Code//
+		System.out.println("Please enter a number:");
+		double extra = sc.nextdouble();
 	}
 }
--- Java/000_Labs/010_random_method-static/base_code/starter.java
+++ Java/000_Labs/010_random_method-static/base_code/starter.java
@@ -9,7 +9,22 @@ import java.util.Random;
 class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
+		
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+
+		int a = (int)(Math.random()*10);
+		int b = (int)(1+Math.random()*101);
+		System.out.println(a);
+		System.out.println(b);
+		double c = (double)(2.5+Math.random()*3.5);
+		double d = (double)(14+Math.random()*14);
+		System.out.println(c);
+		System.out.println(d);
+
+
+		/
+
+
+
 	}
 }

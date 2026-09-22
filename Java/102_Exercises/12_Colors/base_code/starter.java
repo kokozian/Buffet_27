c1cbf8212faa557fff2217406712ec6a68e938f5
Carlo <70654159+kokozian@users.noreply.github.com>
2026-09-22 18:02:33 +0000

    a

--- Java/102_Exercises/12_Colors/base_code/starter.java
+++ Java/102_Exercises/12_Colors/base_code/starter.java
@@ -18,5 +18,7 @@ class starter {
         String resetColor = "\u001B[0m";
         String swatch = startColor + "                    " + resetColor;
         System.out.println(swatch);
+
+        getColor
     }
 }
